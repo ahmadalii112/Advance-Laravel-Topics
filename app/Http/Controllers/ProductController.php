fdf7d79ddc36b754809f8b528ecf011534e418d7
AhmadAlii112 <ahmad.ali78643@gmail.com>
2023-05-27 00:32:30 +0500

    Render Products

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,12 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    public function index(Request $request)
+    /**
+     * @param Request $request
+     * @return Application|Factory|View|\Illuminate\Foundation\Application
+     */
+    public function index(Request $request): \Illuminate\Foundation\Application|View|Factory|Application
     {
-
+        $products = Product::all();
+        return view('products.index', compact('products'));
     }
 }
---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Stripe Payments</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet"/>
+
+
+</head>
+<body>
+<div style="display: flex; gap: 3rem">
+    @foreach($products as $product)
+        <div>
+            <img src="{{ $product->image }}" alt="" style="max-width: 100%">
+            <h5>{{ $product->name }}</h5>
+            <p>${{ $product->price }}</p>
+        </div>
+    @endforeach
+</div>
+<p>
+    <button>Checkout</button>
+</p>
+</body>
+</html>
